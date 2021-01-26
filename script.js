5ebd206067a0bb2803f95899d24864ba25673d2b
Benjamin Megret <benjamin.megret@outlook.fr>
2021-01-26 14:36:57 +0100

    in progress

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-let canvas = document.querySelector('#welcomePage')
+let canvas = document.querySelector('#gamePage')
 let ctx = canvas.getContext('2d')
 
 canvas.style.border = '2px solid black'
 
 let intervalId = 0
 let score = 0
-let start = document.createElement('button')
+let startButton = document.querySelector('#StartButton')
+let loadPage = document.querySelector('#loadingPage')
 let playing = false
 let incrementPlayer = 8
 let playerX = 199
 let playerY = 630
 let isLeftArrow = false;
-let isRightArrow = false
-/*let titan1X = 100
-let titan1Y = 10
-let titan2X = 300
-let titan2Y = 10
-let titan2Increment = 5 */
-titan1Increment = 4
-let isSpaceBar = false;
-let swordY = 650
-let swordX = 199
-let incrementSword = 8
-let titans = [{x: 50, y: 0}]
-let titansIncrement = 5
+let isRightArrow = false;
+let isAttackKey = false;
+//let isSpaceUp = false;
+
+let swords1 = []
+let titans = [{x: 50, y: 100}]
+for (let i = 0; i < titans.length; i++){
+    titans[i] = {x: 50, y: -100, status: 1}
+}
+
 
 let welcomeImg = document.createElement('img')
 welcomeImg.src = '/images/welcome1.jpg'
@@ -41,8 +39,22 @@ titan2.src = '/images/titan2.png'
 let sword = document.createElement('img')
 sword.src = '/images/sword.png'
 
+
+
+document.addEventListener('keyup', (event) => {
+    isRightArrow = false;
+    isLeftArrow = false;
+    isAttackKey = false
+    //isSpaceUp = false
+})
+
 document.addEventListener('keydown', (event) => {
-    if (event.keyCode == 39 || event.key == "ArrowRight"){
+    if (event.keycode == 32 || event.code == "Space") {
+       isAttackKey = true;
+       console.log('attack')
+    }
+   
+    else if (event.keyCode == 39 || event.key == "ArrowRight"){
         isRightArrow = true;
         isLeftArrow = false;
     }
@@ -50,30 +62,19 @@ document.addEventListener('keydown', (event) => {
         isRightArrow = false;
         isLeftArrow = true;
     }
+
 })
 
-document.addEventListener('keyup', (event) => {
-    isRightArrow = false;
-    isLeftArrow = false;
-})
-
-document.addEventListener('mousedown', () => {
-    incrementSword = -8
-})
 
 function swords (){
-    ctx.drawImage(sword, swordX, swordY)
-
-    if (isRightArrow && (swordX + sword.width < canvas.width)) {
-        swordX += incrementSword
-
-    }
-    else if (isLeftArrow && swordX > 0) {
-        swordX -= incrementSword
-
+    if (isAttackKey){
+        swords1.push({x: playerX, y: playerY})
     }
 
-
+    for (let i = 0; i < swords1.length; i++){
+        ctx.drawImage(sword, swords1[i].x, swords1[i].y)
+        swords1[i].y--
+    }
 }
 
 function mikasa (){
@@ -92,50 +93,38 @@ function mikasa (){
 
 function titansFall () {
 
-    let constant = titan1.width + 100
+    //let constant = titan1.width + 100
     for (let i = 0; i < titans.length; i++) {
         ctx.drawImage(titan1, titans[i].x, titans[i].y)
         //ctx.drawImage(titan2, titans[i].x + constant, titans[i].y)
         titans[i].y++
-        if (titans[i].y == canvas.height - player.height){
+        if (titans[i].y == canvas.height / 3){
             titans.push({
-                x: Math.floor(Math.random() * canvas.width),
+                x: Math.floor(Math.random() * (canvas.width - titan1.width)),
                 y: -50
             })
         }
     }
-
-
-
-    //ctx.drawImage(titan1, titan1X, titan1Y)
-    //ctx.drawImage(titan2, titan2X, titan2Y)
-
-    //titan1Y += titan1Increment
-    //titan2Y += titan2Increment
 }
 
 
 
-function titansCollision1 (){
+/*function titansCollision1 (){
+    for (let i = 0; i < titans.length; i++){
+        if (titans[i].y + titansCollision1.height == canvas.height){
+            clearInterval(intervalId);
+            alert('gameover');
+        }
+    }
+}*/
 
+function loadingPage(){
+    ctx.loadPage
+    ctx.startButton
+    draw()
+    text()
 }
 
-
-
-function gamePlay (){
-    ctx.drawImage(gamePage, 0, 0)
-    titansFall()
-    swords()
-    mikasa()
-    swords()
-    titansCollision1()
-    
-}
-
-
-
-
-/*
 function draw (){
     ctx.drawImage(welcomeImg, 0, 0)
 }
@@ -167,10 +156,14 @@ function text (){
     ctx.closePath()
 }
 
-function startButton (){
-    start('/images/start.png', 120, 120)
+
+function gamePlay (){
+    ctx.drawImage(gamePage, 0, 0)
+    titansFall()
+    swords()
+    mikasa()
 }
-*/
+
 
 
 intervalId = setInterval(() => {
@@ -181,4 +174,4 @@ intervalId = setInterval(() => {
     //requestAnimationFrame(mikasa)
     //requestAnimationFrame(titansFall)
     //requestAnimationFrame(titansCollision1)
-}, 50) 
+}, 200) 
