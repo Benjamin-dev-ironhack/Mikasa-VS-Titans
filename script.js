35e4f24fd99ccf88de9e21d0f060267abe96a3bb
Benjamin Megret <benjamin.megret@outlook.fr>
2021-01-27 12:27:46 +0100

    in progress

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ let intervalId = 0
 let score = 0
 let startBtn = document.getElementById('startButton')
 let loadPage = document.getElementById('loadingPage')
-//loadPage.style.display = 'none'
+let gameOverPage = document.getElementById('gameOver')
+let restartBtn = document.getElementById('restartBtn')
 let incrementPlayer = 8
 let playerX = 199
 let playerY = 630
@@ -17,7 +18,7 @@ let isAttackKey = false;
 //let isSpaceUp = false;
 
 let swords1 = []
-let titans = [{x: 50, y: -100}]
+let titans = [{x: 50, y: -200}]
 
 let welcomeImg = document.createElement('img')
 welcomeImg.src = '/images/welcome1.jpg'
@@ -47,7 +48,6 @@ document.addEventListener('keyup', (event) => {
 document.addEventListener('keydown', (event) => {
     if (event.keycode == 32 || event.code == "Space") {
        isAttackKey = true;
-       console.log('attack')
     }
    
     else if (event.keyCode == 39 || event.key == "ArrowRight"){
@@ -65,6 +65,7 @@ document.addEventListener('keydown', (event) => {
 function swords (){
     if (isAttackKey){
         swords1.push({x: playerX, y: playerY})
+        isAttackKey = false
     }
 
     for (let i = 0; i < swords1.length; i++){
@@ -95,80 +96,40 @@ function titansFall () {
         ctx.drawImage(titan1, titans[i].x, titans[i].y)
         //ctx.drawImage(titan2, titans[i].x + 100, titans[i].y)
         titans[i].y++
-        if (titans[i].y == 100){
+        if (titans[i].y == 0){
             titans.push({
                 x: Math.floor(Math.random() * (canvas.width - titan1.width)),
-                y: -50
+                y: -200
             })
         }
     }
 }
 
-
+//else if ((swords1[j].x > titans[i].x && swords1[j].x + sword.width < titans[i].x + titan1.width) && 
+//            (titans[i].y + titan1.height > swords1[j].y) ){
 
 function titansCollision (){
     for (let i = 0; i < titans.length; i++){
         for (let j = 0; j < swords1.length; j++){
-            //console.log(swords1[j].x > titans[i].x && swords1[j].x + sword.width < titans[i].x + titan1.width)
-            //console.log(titans[i].y , titan1.height , canvas.height)
-            if (titans[i].y + titan1.height > canvas.height && (titans[i].y + titan1.height < 2500)){
+            if (titans[i].y + titan1.height > canvas.height){
                 clearInterval(intervalId)
-                location.reload()
+                //alert('game over')
+                gameOver()
             }
-            else if ((swords1[j].x > titans[i].x && swords1[j].x + sword.width < titans[i].x + titan1.width) && 
+            else if ((swords1[j].x < titans[i].y + titan1.height && swords1[j].x + sword.width < titans[i].x + titan1.width + titan1.height) && 
             (titans[i].y + titan1.height > swords1[j].y) ){
-                // TODO: remove the titan by splicing or changing th coordinates
-                // ze change the y of titan to 3000 to put it outside the canvas
-                // you must try splice here
-                titans[i].y = 3000 
-                swords1[j].y = 3000
+                titans.splice(i, 1)
+                swords1.splice(j, 1)
                 score ++
             }
         }
     }
     ctx.beginPath()
     ctx.font = '30px Verdana red'
-    ctx.fillText('Score: ' + score, 10, 30)
+    ctx.fillText('Titans killed: ' + score, 10, 30)
     ctx.fillStyle = '#571e1e'
     ctx.closePath()
 }
-
-function loadingPage(){
-    draw()
-    text()
-}
-
-function draw(){
-    ctx.drawImage(welcomeImg, 0, 0)
-}
-
-function text (){
-    ctx.beginPath()
-    ctx.font = 'bold 45px Verdana red'
-    ctx.fillText('MIKASA VS TITANS', 35, 80)
-    ctx.fillStyle = '#571e1e'
-    ctx.closePath()
-    
-    ctx.beginPath()
-    ctx.font = '25px Verdana red'
-    ctx.fillText('In this game you will help Mikasa to ', 60,  150)
-    ctx.fillText('protect the wall !', 160, 175 )
-    ctx.closePath()
-
-    ctx.beginPath()
-    ctx.font = 'bold 25px Verdana red'
-    ctx.fillText('INSTRUCTIONS', 160,  550)
-    ctx.closePath()
-
-    ctx.beginPath()
-    ctx.font = '25px Verdana red'
-    ctx.fillText('Arrow left --> you go left', 120, 575)
-    ctx.fillText('Arrow right --> you go right', 120, 595)
-    ctx.fillText('Space --> you launch sword', 120, 615)
-    ctx.fillText('Mouse Click --> melee attack', 120, 635)
-    ctx.closePath()
-}
-
 
 function gamePlay (){
     ctx.drawImage(gamePage, 0, 0)
@@ -182,14 +143,26 @@ function startGame(){
     canvas.style.display = 'block'
     startBtn.style.display = 'none' 
     loadPage.style.display = 'none'
+    gameOverPage.style.display = 'none'
     intervalId = setInterval(() => {
         requestAnimationFrame(gamePlay)
     }, 30) 
 }
 
-window.addEventListener('load', () => {
+function gameOver(){
     canvas.style.display = 'none'
+    startBtn.style.display = 'none' 
+    loadPage.style.display = 'none'
+    gameOverPage.style.display = 'block'
+    restartBtn.addEventListener('click', () => {
+        startGame()
+    })
+}
+
+window.addEventListener('load', () => {
     loadPage.style.display = 'block'
+    canvas.style.display = 'none'
+    gameOverPage.style.display = 'none'
 
     startBtn.addEventListener('click', () => {
         startGame()
